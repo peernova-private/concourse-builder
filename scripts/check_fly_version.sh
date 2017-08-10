7b76edc791f4505a7d0342ca9b3c4984e0aabe4e
Ilja Kravchenko <ilya@peernova.com>
2017-08-10 18:51:53 +0300

    added option for insecure connection to concourse

--- scripts/check_fly_version.sh
+++ scripts/check_fly_version.sh
@@ -2,8 +2,16 @@
 
 set -ex
 
+INSECURE="1"
+
+if [ ! -z "$INSECURE" ]
+then
+  INSECUR_VAR="-k"
+fi
+
+CONCOURSE_URL="https://52.203.12.116"
 FLY_VERSION=`fly -v`
-CONCOURSE_VERSION=`curl $CONCOURSE_URL/api/v1/info | awk -F ',' ' { print $1 } ' | awk -F ':' ' { print $2 } ' | sed -e 's/^"//' -e 's/"$//'`
+CONCOURSE_VERSION=`curl $INSECUR_VAR $CONCOURSE_URL/api/v1/info | awk -F ',' ' { print $1 } ' | awk -F ':' ' { print $2 } ' | sed -e 's/^"//' -e 's/"$//'`
 
 if [ ! "$FLY_VERSION" == "$CONCOURSE_VERSION" ]
 then
